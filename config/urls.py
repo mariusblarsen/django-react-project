ebd56b18312e24fae7ba0ae8a9ce375f696d3eb6
mariusblarsen <mariubla@stud.ntnu.no>
2021-02-10 16:11:50 +0100

    django url only for builtin admin page

--- config/urls.py
+++ config/urls.py
@@ -19,6 +19,5 @@ from django.urls import path
 from django.views.generic import TemplateView
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
-    url(r'^', TemplateView.as_view(template_name="index.html")),
+    path('admin/', admin.site.urls),
 ]
