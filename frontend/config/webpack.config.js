1929b2b7ff4819812603cdcd86be9083fe016bbb
mariusblarsen <mariubla@stud.ntnu.no>
2021-02-16 02:00:44 +0100

    removed unused functionalities

--- frontend/config/webpack.config.js
+++ frontend/config/webpack.config.js
@@ -26,7 +26,6 @@ const ModuleNotFoundPlugin = require('react-dev-utils/ModuleNotFoundPlugin');
 const ForkTsCheckerWebpackPlugin = require('react-dev-utils/ForkTsCheckerWebpackPlugin');
 const typescriptFormatter = require('react-dev-utils/typescriptFormatter');
 const ReactRefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin');
-const BundleTracker = require('webpack-bundle-tracker');
 
 const postcssNormalize = require('postcss-normalize');
 
