57a85b39195ce1d6ea1e46744c8946ecfd9e82c8
mariusblarsen <mariubla@stud.ntnu.no>
2021-02-18 01:15:43 +0100

    added model Ticker

--- api/models.py
+++ api/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Ticker(models.Model):
+    created = models.DateTimeField(auto_now_add=True)
+    symbol = models.CharField(max_length=10)
+    name = models.CharField(max_length=50)
+    
+    class Meta:
+        unique_together = ["symbol", ]
+
+    def __str__(self):
+        return "{} {}".format(self.symbol, self.name)
+    
+
