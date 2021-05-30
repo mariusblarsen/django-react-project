9082da8b607132332a28f412ec69067f764a2e3c
Marius Larsen <mariubla@stud.ntnu.no>
2021-05-30 22:35:07 +0200

    simple test for ticker-model

--- api/tests.py
+++ api/tests.py
@@ -1,3 +1,18 @@
 from django.test import TestCase
 
-# Create your tests here.
+from .models import Ticker
+
+TICKER_SYMBOL = "TEST"
+TICKER_NAME = "Test ticker"
+
+class TickerTestCase(TestCase):
+    def setUp(self):
+        Ticker.objects.create(
+            symbol=TICKER_SYMBOL,
+            name=TICKER_NAME
+        )
+    
+    def test_name(self):
+        ticker = Ticker.objects.get(symbol=TICKER_SYMBOL)
+        self.assertEqual(ticker.symbol, TICKER_SYMBOL)
+
